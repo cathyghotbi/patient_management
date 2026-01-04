4ad56864fcd4a3d9f5afef703768a15c38afa336
cathyghotbi <cathyghotbi@yahoo.com>
2026-01-04 11:10:41 +0100

    Create menu.py

---
+++ ui/menu.py
@@ -0,0 +1,9 @@
+def menu():
+    print("\n--- Patient Management System ---")
+    print("1. View all patients")
+    print("2. Search by name")
+    print("3. Search by disease")
+    print("4. Filter by minimum age")
+    print("5. Group patients by disease")
+    print("6. Save grouped data (JSON & CSV)")
+    print("0. Exit")
